cc3e5489a0595da0d1ae620844c4491b2e081949
sandrinio <sandro.suladze@gmail.com>
2018-03-23 00:20:25 +0400

    fixed

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,8 +3,8 @@ import store from '@/store/store'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://localhost:3000/',
-    // baseURL: 'http://46.101.238.60/',
+    // baseURL: 'http://localhost:3000/',
+    baseURL: 'http://46.101.238.60/',
     headers: {
       Authorization: `Bearer ${store.state.token}`
     }
